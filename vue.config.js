f858f1edc0bedde1bcdba77000199aac5006c627
lhb1032352370 <1032352370@qq.com>
2019-10-30 13:02:11 +0800

    'docs'

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 // vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-todo/'
+    : '/',
   devServer: {
     overlay: {
       warnings: true,
       error: false,
     },
-  },
+  }
 };
